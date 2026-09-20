eee23973e96cb8009f9f43ea74b9a76ef45ec401
Skye Grady <skyegrady16@gmail.com>
2026-09-20 18:41:51 -0500

    update to header and assets files

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.png";
+
+function Header() {
+  return (
+    <header className="header">
+      <img src={logo} className="header__logo"></img>
+      <p className="header__date-and-location">DATE, LOCATION</p>
+      <buttion className="header__add-clothes-btn">+ Add Clothes</buttion>
+      <div className="header__user-container">
+        <p className="header__username">Terrence Tegegne</p>
+        <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
