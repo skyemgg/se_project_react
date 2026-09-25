6d8fc1bce71d37990d1f94cba79c35f2fc388528
Skye Grady <skyegrady16@gmail.com>
2026-09-25 16:52:29 -0500

    ModalWithForm and App.jsx

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,30 +1,20 @@
-function ModalWithForm() {
+import "./ModalWithForm.css";
+
+function ModalWithForm({ childern, buttonText, title }) {
   return (
     <div className="modal">
-      <form className="modal__form">
-        <h2 className="modal__title">New garment</h2>
+      <div className="modal__content">
+        <h2 className="modal__title">{title}</h2>
         <button type="button" className="modal__close">
           CLOSE
         </button>
-        <label htmlFor="name" className="modal__label">
-          Name{""}
-          <input
-            type="text"
-            className="modal__input"
-            id="name"
-            placeholder="Name"
-          />
-        </label>
-        <label htmlFor="imageUrl" className="modal__label">
-          Image{""}
-          <input
-            type="text"
-            className="modal__input"
-            id="imageUrl"
-            placeholder="Image URL"
-          />
-        </label>
-      </form>
+        <form className="modal__form">
+          {childern}
+          <button className="modal__submit" type="submit">
+            {buttonText}
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
